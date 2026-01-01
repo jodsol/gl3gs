8122e425fa41f3ebbaa660c5ba7193b382244af3
jodsol <jodsol94@gmail.com>
2026-01-01 16:42:47 +0900

    chore: f5로 디버깅 설정 추가

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+REM 1. MSVC 환경 불러오기
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
+
+REM 2. CMake Configure (Ninja)
+cmake -S . -B build -G Ninja
+
+REM 3. CMake Build
+cmake --build build --config Debug
